58d61c32fcd0bd65bcfc54efb9f1c60f9b3edfcc
monako97 <poi.nyaa@qq.com>
2021-01-07 00:56:21 +0800

    feat: markedor

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export { default as MarkDown } from './markdown';
 export { MarkDownEdit } from './markdown-edit';
 export { BubblyButton } from './bubbly-button';
 export { default as Button } from './button';
-export { default as utils } from './utils';
+export { markdownUtil, getDefaultTheme, getMaxZindex, openPanel, useTheme } from './utils';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,7 @@
 import markdownUtil from './markdown-util';
 import getDefaultTheme from './get-default-theme';
-import getMaxZIndex from './get-max-zIndex';
+import getMaxZindex from './get-max-zIndex';
 import openPanel from './open-panel';
 import useTheme from './use-theme';
 
-const utils = { markdownUtil, getDefaultTheme, getMaxZIndex, openPanel, useTheme };
-
-export default utils;
+export { markdownUtil, getDefaultTheme, getMaxZindex, openPanel, useTheme };
